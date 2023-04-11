29b2cb9cb3f11d575ff8eb2c57bba0613b2f56e1
Yash dhadve <yashdhadwe@gmail.com>
2023-04-11 18:59:00 +0530

    base

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const ejs = require('ejs')
+ 
+const app = express()
+
+app.set('view engine', 'ejs')
+app.listen(4000)
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/EigenValuesNVectors', (req, res) => {
+    res.render('EigenValuesNVectors')
+})
+
+app.get('/QuadraticEquations', (req, res) => {
+    res.render('QuadraticEquations')
+})
+
+app.use((req, res) => {
+    res.status(404).render('404')
+})
